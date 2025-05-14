c37dce45283a8ecbf73c2e76d8e46a57338d1726
Marifus <mehmetariffy@gmail.com>
2025-05-14 17:20:55 +0300

    tf2 kutuphanesi

--- include/pp_pkg/controller.hpp
+++ include/pp_pkg/controller.hpp
@@ -7,36 +7,43 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <autoware_msgs/VehicleCmd.h>
 #include <cmath>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/LinearMath/Transform.h>
+#include <visualization_msgs/Marker.h>
 
 
 namespace purepursuit {
 
     class PurePursuit
     {
+        bool input_log, output_log;
 
         ros::NodeHandle& nh_;
         ros::Subscriber path_sub;
         ros::Subscriber odom_sub;
         ros::Publisher ctrl_pub;
         ros::Publisher path_pub;
+        ros::Publisher mark_pub;
 
         double lookahead_distance;
-        double bicycle_length;
+        double axle_length;
         double current_heading;
+        int marker_id;
 
         nav_msgs::Path path;
         nav_msgs::Odometry vehicle_odom;
 
-        double velocity = 10;
+        double velocity;
 
         bool ReadParameters();
-        void PathCallback(const nav_msgs::Odometry::ConstPtr& msg);
+/*         void PathCallback(const nav_msgs::Odometry::ConstPtr& msg); */
+        void PathCallback(const nav_msgs::Path::ConstPtr& msg);
         void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg);
         void ControlOutput();
-        void LocalTransform(geometry_msgs::Pose& current_point_pose, geometry_msgs::Pose& target_point_pose, double transformed_vector[3]);
         int ClosestWaypointIndex(geometry_msgs::Pose& current_point_pose, nav_msgs::Path& path);
         geometry_msgs::Pose ChooseLookaheadPoint(geometry_msgs::Pose& current_point_pose, nav_msgs::Path& path, double t_lookahead_distance, int closest_point_index);
         double PurePursuitAlgorithm(double target_x, double target_y, double length);
+        geometry_msgs::Pose LocalTransform(const geometry_msgs::Pose& origin_pose, const geometry_msgs::Pose& target_point_pose);
 
         public:
 
--- src/controller.cpp
+++ src/controller.cpp
@@ -12,10 +12,13 @@ namespace purepursuit
             ros::requestShutdown();
         }
 
+        marker_id = 0;
+
         path_sub = nh_.subscribe("/shortest_path", 10, &PurePursuit::PathCallback, this);
         odom_sub = nh_.subscribe("/odom", 10, &PurePursuit::OdomCallback, this);
         ctrl_pub = nh_.advertise<autoware_msgs::VehicleCmd>("/vehicle_cmd", 10);
         path_pub = nh_.advertise<nav_msgs::Path>("/path", 10);        
+        mark_pub = nh_.advertise<visualization_msgs::Marker>("/waypoint", 10);
 
     }
 
@@ -23,16 +26,21 @@ namespace purepursuit
     bool PurePursuit::ReadParameters()
     {
         if (!nh_.getParam("lookahead_distance", lookahead_distance)) return false;
-        if (!nh_.getParam("bicycle_length", bicycle_length)) return false;
+        if (!nh_.getParam("axle_length", axle_length)) return false;
+        if (!nh_.getParam("velocity", velocity)) return false;
+        if (!nh_.getParam("input_log", input_log)) return false;
+        if (!nh_.getParam("output_log", output_log)) return false;
 
         ROS_INFO("Lookahead Distance: [%f]", lookahead_distance);
-        ROS_INFO("Sase Uzunlugu: [%f]", bicycle_length);
+        ROS_INFO("Sase Uzunlugu: [%f]", axle_length);
+        ROS_INFO("Arac Hizi: [%f]", velocity);
+        ROS_INFO("Girdi/Cikti Gosterme: [%d, %d]", input_log, output_log);
 
         return true;
     }
 
 
-    void PurePursuit::PathCallback(const nav_msgs::Odometry::ConstPtr& msg)
+/*     void PurePursuit::PathCallback(const nav_msgs::Odometry::ConstPtr& msg)
     {
         geometry_msgs::PoseStamped pose_stamped;
 
@@ -42,6 +50,24 @@ namespace purepursuit
         path.poses.push_back(pose_stamped);
         path.header = msg->header;
         path_pub.publish(path);
+    } */
+
+
+    void PurePursuit::PathCallback(const nav_msgs::Path::ConstPtr& msg)
+    {
+        path = *msg;
+
+        for (int i = 0; i < path.poses.size(); ++i)
+        {
+            if (i+1<path.poses.size())
+            {
+                while (path.poses[i].pose.position.x == path.poses[i+1].pose.position.x && path.poses[i].pose.position.y == path.poses[i+1].pose.position.y)
+                {
+                    path.poses.erase(path.poses.begin()+i);
+                    if (i+1>=path.poses.size()) break;
+                }
+            }
+        }
     }
 
 
@@ -49,11 +75,17 @@ namespace purepursuit
     {
         vehicle_odom = *msg;
 
+        vehicle_odom.pose.pose.position.x += cos(current_heading)*axle_length*0;
+        vehicle_odom.pose.pose.position.y += sin(current_heading)*axle_length*0;
+
         geometry_msgs::Quaternion& q = vehicle_odom.pose.pose.orientation;
         current_heading = atan2(2.0 * (q.w * q.z + q.x * q.y) , 1.0 - 2.0 * (q.y * q.y + q.z * q.z));
 
-        ROS_INFO("Simdiki Konum: [%f, %f]", vehicle_odom.pose.pose.position.x, vehicle_odom.pose.pose.position.y);
-        ROS_INFO("Simdiki Yaw: [%f]", current_heading);
+        if(input_log)
+        {
+            ROS_INFO("Simdiki Konum: [%f, %f]", vehicle_odom.pose.pose.position.x, vehicle_odom.pose.pose.position.y);
+            ROS_INFO("Simdiki Yaw: [%f]", current_heading);
+        }
 
         if (path.poses.size() != 0) ControlOutput();
     }
@@ -62,11 +94,10 @@ namespace purepursuit
     void PurePursuit::ControlOutput()
     {
         geometry_msgs::Pose target_pose = ChooseLookaheadPoint(vehicle_odom.pose.pose, path, lookahead_distance, ClosestWaypointIndex(vehicle_odom.pose.pose, path));
-        double transformed_vec[3] = {0, 0, 0};
 
-        LocalTransform(vehicle_odom.pose.pose, target_pose, transformed_vec);
+        geometry_msgs::Pose transformed_target_pose = LocalTransform(vehicle_odom.pose.pose, target_pose);
 
-        double steering_angle = PurePursuitAlgorithm(transformed_vec[0], transformed_vec[1], bicycle_length);
+        double steering_angle = PurePursuitAlgorithm(transformed_target_pose.position.x, transformed_target_pose.position.y, axle_length);
 
         double steering_angle_degree = steering_angle * (180 / M_PI);
 
@@ -82,44 +113,15 @@ namespace purepursuit
 
         }
 
-        ROS_INFO("Donus Acisi: [%f]", steering_angle);
+        if(output_log)
+        {
+            ROS_INFO("Donus Acisi: [%f]", steering_angle);
+        }
 
         autoware_msgs::VehicleCmd control_msg;
         control_msg.twist_cmd.twist.angular.z = steering_angle;
         control_msg.twist_cmd.twist.linear.x = velocity;
         ctrl_pub.publish(control_msg);
-    }
-
-
-    void PurePursuit::LocalTransform(geometry_msgs::Pose& current_point_pose, geometry_msgs::Pose& target_point_pose, double transformed_vector[3])
-    {
-        
-        double tx = -1 * current_point_pose.position.x;
-        double ty = -1 * current_point_pose.position.y;
-
-        geometry_msgs::Quaternion& q = current_point_pose.orientation;
-        double current_heading_ = atan2(2.0 * (q.w * q.z + q.x * q.y) , 1.0 - 2.0 * (q.y * q.y + q.z * q.z));
-/*         double current_heading_ = tf::getYaw(current_point_pose.orientation); */
-
-        double target_vec[3] = {target_point_pose.position.x, target_point_pose.position.y, 1};
-
-        double TransformationMatrix[3][3] = {
-            {cos(-current_heading_), -sin(-current_heading_), cos(-current_heading_) * tx - sin(-current_heading_) * ty},
-            {sin(-current_heading_), cos(-current_heading_), sin(-current_heading_) * tx + cos(-current_heading_) * ty},
-            {0.0, 0.0, 1.0}
-        };
-
-        for (int i=0; i<3; i++) {
-        
-            transformed_vector[i] = 0;
-
-            for (int j=0; j<3; j++) {
-
-                transformed_vector[i] += TransformationMatrix[i][j] * target_vec[j];
-
-           }
-        }
-
     }
 
 
@@ -154,26 +156,30 @@ namespace purepursuit
     }
 
     
-    geometry_msgs::Pose PurePursuit::ChooseLookaheadPoint(geometry_msgs::Pose& current_point_pose, nav_msgs::Path& path, double t_lookahead_distance, int closest_point_index) 
+    geometry_msgs::Pose PurePursuit::ChooseLookaheadPoint(geometry_msgs::Pose& current_point_pose, nav_msgs::Path& t_path, double t_lookahead_distance, int closest_point_index) 
     {
 
         int chosen_point_index = closest_point_index;
-        geometry_msgs::PoseStamped& waypoint = path.poses[chosen_point_index];
+        geometry_msgs::PoseStamped& waypoint = t_path.poses[chosen_point_index];
 
         double distance = sqrt(std::pow((waypoint.pose.position.x - current_point_pose.position.x), 2) + std::pow((waypoint.pose.position.y - current_point_pose.position.y), 2));
 
         while (t_lookahead_distance > distance) 
         {  
-            chosen_point_index++;
-            if (chosen_point_index<path.poses.size()) 
+
+            if (++chosen_point_index<t_path.poses.size()) 
             {
-            geometry_msgs::PoseStamped& waypoint = path.poses[chosen_point_index];
+            geometry_msgs::PoseStamped& waypoint = t_path.poses[chosen_point_index];
             distance = sqrt(std::pow((waypoint.pose.position.x - current_point_pose.position.x), 2) + std::pow((waypoint.pose.position.y - current_point_pose.position.y), 2));
             }
-            else break;
-        }
 
-        chosen_point_index--;
+            else 
+            {
+                chosen_point_index = t_path.poses.size()-1;
+                break;
+            }
+
+        }
 
 /* 
         for (int i = closest_point_index; i < path.poses.size(); ++i)
@@ -198,7 +204,24 @@ namespace purepursuit
         }
 */
 
-        geometry_msgs::Pose& chosen_point = path.poses[chosen_point_index].pose;
+        geometry_msgs::Pose& chosen_point = t_path.poses[chosen_point_index].pose;
+
+        visualization_msgs::Marker marker;
+        marker.header = t_path.header;
+        marker.id = marker_id;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position = chosen_point.position;
+        marker.type = visualization_msgs::Marker::SPHERE;
+        marker.scale.x = 0.15;
+        marker.scale.y = 0.15;
+        marker.scale.z = 0.15;
+        marker.color.a = 1.0;
+        marker.color.r = 0.0;
+        marker.color.g = 0.0;
+        marker.color.b = 1.0;
+        
+        mark_pub.publish(marker);
+
         return chosen_point;
     }
 
@@ -210,6 +233,24 @@ namespace purepursuit
         double steering = atan2((2*target_y*length), (distance2));
 
         return steering;
+    }
+
+
+    geometry_msgs::Pose PurePursuit::LocalTransform(const geometry_msgs::Pose& origin_pose, const geometry_msgs::Pose& target_point_pose)
+    {   
+        tf2::Transform origin_tf, target_point_tf, g2l_transform, transformed_point_tf;
+
+        tf2::fromMsg(origin_pose, origin_tf);
+        g2l_transform = origin_tf.inverse();
+
+        tf2::fromMsg(target_point_pose, target_point_tf);
+
+        transformed_point_tf = g2l_transform * target_point_tf;
+        
+        geometry_msgs::Pose transformed_pose;
+        tf2::toMsg(transformed_point_tf, transformed_pose);
+
+        return transformed_pose;
     }
 
 }
